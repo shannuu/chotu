47098b2e59fca0a93322873804741a3bbc0b5ef9
Kushal <64453862+shannuu@users.noreply.github.com>
2022-06-09 18:27:28 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,14 +4,17 @@ lversion=$(cat lversion || exit)
 
 if [[ $pversion != $lversion ]]; then
     cd
+    clear
     echo "[*] Found a new version of CHOTU"
     sleep 0.5
     echo "Auto Updating"
     sleep 2
     rm -rf chotu
     git clone https://github.com/shannuu/chotu
+    clear
     echo "[*] Restart CHOTU to apply changes"
     sleep 3
+    cd
     clear
     exit
 fi
