6a7a0eb12b08d896125ce3ed29a43121b1e00104
Kushal <64453862+shannuu@users.noreply.github.com>
2022-06-10 15:23:47 +0530

    Add files via upload

---
+++ chotu-2.sh
@@ -0,0 +1,18 @@
+# chotu 2.0 
+# made by kushal
+dir="$HOME/chotu/utils"
+
+source $dir/install.sh
+source $dir/listen.sh
+source $dir/speak.sh
+#source $dir/update.sh
+
+while [[ true ]]; do
+    if [[ $cnf == "true" ]]; then
+        cnf="false"
+    elif [[ $cnf != "true" ]]; then
+        listenf
+    fi
+    source $dir/checkcommand.sh
+    sleep 3
+done
