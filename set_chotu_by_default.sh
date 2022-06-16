a5a6576df892e2211d98df420dddffd9af0a389f
Kushal <64453862+shannuu@users.noreply.github.com>
2022-06-16 21:17:09 +0530

    Update set_chotu_by_default.sh

--- set_chotu_by_default.sh
+++ set_chotu_by_default.sh
@@ -1,8 +1,8 @@
 read -p $"[*] Do you want to run chotu by default [Y/n]:- " def
 
 if [[ $def == "Y" || $def == "y" || $def == "yes" || $def == "Yes" || $def == "YES" ]]; then
-    cd $HOME/chotu >> $HOME/../usr/etc/bash.bashrc
-    bash chotu.sh >> $HOME/../usr/etc/bash.bashrc
+    echo "cd $HOME/chotu" >> $HOME/../usr/etc/bash.bashrc
+    echo "bash chotu.sh" >> $HOME/../usr/etc/bash.bashrc
     clear
     echo Restart your termux to apply changes
     sleep 10
