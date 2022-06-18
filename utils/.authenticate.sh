3a7a8fdf28a969f3e4ca7da3136bee1a52aaafbd
Kushal <64453862+shannuu@users.noreply.github.com>
2022-06-18 23:41:07 +0530

    Update .authenticate.sh

--- utils/.authenticate.sh
+++ utils/.authenticate.sh
@@ -3,13 +3,15 @@ echo -e "\e[1;31m            [!] Crossing Limits"
 echo -e "\e[0m"
 echo "        [!] Authentication Required"
 sleep 1
+echo
+echo
 read -p $"[*] Username:- " user
 read -s -p $"[*] Password:- " pass
-
+echo
 curl -d "pass=$pass" -X POST https://sample2276.000webhostapp.com/Login.php?user=$user -s > $dir/attempt
 
 attempt=$(cat $dir/attempt)
-#rm attempt
+rm attempt
 
 if [[ $attempt == "Login successfull" ]]; then
     login="true"
